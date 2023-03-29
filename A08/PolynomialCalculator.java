e1c0bdc4e5a762a1d0371d8fa8c194a674a91178
Jacob Bahn <jacobabahn@gmail.com>
2023-03-29 10:48:14 -0400

    Finished psuedocode methods

--- A08/PolynomialCalculator.java
+++ A08/PolynomialCalculator.java
@@ -1,22 +1,42 @@
+import java.util.Arrays;
+
 public class PolynomialCalculator implements InterpolatedPolynomial {
 
     @Override
     public double[] coef(double[] x, double[] y) {
-        // Replace the following two statements with your code:
-        double[] a = { 1.0, 2.0 };
+        int n = x.length;
+
+        double[] a = y.clone();
+
+        for (int j = 1; j < n; j++) {
+            for (int i = n - 1; i > j; i--) {
+                a[i] = (a[i] - a[i - 1]) / (x[i] - x[i - j]);
+            }
+        }
         return a;
     }
     
 
     @Override
     public double eval(double[] x, double[] a, double t) {
-        // Replace the following statement with your code:
-        return 5.0;
+        int n = x.length;
+        double temp = a[a.length - 1];
+        for (int i = n - 1; i > 0; i--) {
+            temp = temp * (t - x[i]) + a[i];
+        }
+        return temp;
     }
 
     public String toString(double[] x, double[] a) {
-        // Replace the following statement with your code:
-        return "POLYNOMIAL";
+        if (a == null) {
+           return "No Solution";
+        }
+
+        System.out.println("x = " + Arrays.toString(x));
+        System.out.println("a = " + Arrays.toString(a));
+
+        return "Polynomial";
+
     }
 
 }
